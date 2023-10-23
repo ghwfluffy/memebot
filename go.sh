ee03d38630cf1f4c5030cb53a6364c4523479809
GhwFluffy <ghw@ghwfluffy.com>
2023-10-22 21:49:18 -0500

    Initial commit

---
+++ go.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source Venv.source
+python ./test.py
+
---
+++ test.py
@@ -0,0 +1,132 @@
+import openai
+
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+img_file = "memes/distracted_boyfriend/DistractedBoyfriend.png"
+img = Image.open(img_file)
+draw = ImageDraw.Draw(img)
+
+FONT = "/usr/share/fonts/truetype/tlwg/Loma.ttf"
+
+texts = [
+    {
+        "field": "New Thing",
+        "meta": "The thing that is being covetted",
+        "x": 420,
+        "y": 1100,
+        "x2": 975,
+        "y2": 1320,
+    },
+    {
+        "field": "Subject",
+        "meta": "The subject that is covetting something",
+        "x": 1299,
+        "y": 735,
+        "x2": 1788,
+        "y2": 957,
+    },
+    {
+        "field": "Old Thing",
+        "meta": "The thing being cast asside, forgotten, replaced",
+        "x": 1872,
+        "y": 855,
+        "x2": 2268,
+        "y2": 1053,
+    },
+]
+
+filler_text = [
+    {
+        "field": "New Thing",
+        "text": "I very much like burgers they are very good and tasty",
+        #"text": "Burger",
+    },
+    {
+        "field": "Subject",
+        "text": "Me",
+    },
+    {
+        "field": "Old Thing",
+        "text": "The diet I told myself I would stick to but you know that's not happening",
+        #"text": "Diet",
+    },
+]
+
+def get_longest_line(lines):
+    if len(lines) == 1:
+        return lines[0]
+    font = ImageFont.truetype(FONT, 12)
+    longest = 0
+    idx = -1
+    for i in range(len(lines)):
+        text_width = draw.textlength(lines[i], font=font)
+        if text_width > longest:
+            idx = i
+            longest = text_width
+    return lines[idx]
+
+def get_text_width(text, font_size):
+    font = ImageFont.truetype(FONT, font_size)
+    return draw.textlength(text, font=font)
+
+def get_text_height(text, font_size):
+    font = ImageFont.truetype(FONT, font_size)
+    return font.getmetrics()[0]
+
+def calculate_lines(text, width, height):
+    font_size = 1
+    line_length = len(text)
+    num_lines = 1
+    while True:
+        lines = textwrap.TextWrapper(width=line_length, break_long_words=False).wrap(text=text)
+
+        longest_line = get_longest_line(lines)
+
+        if len(lines) > num_lines and (get_text_height(longest_line, font_size) * len(lines)) > height:
+            line_length += 1
+            break
+
+        while get_text_width(longest_line, font_size + 1) < width:
+            font_size += 1
+
+        if line_length == 1:
+            break
+        line_length -= 1
+
+    lines = textwrap.TextWrapper(width=line_length, break_long_words=False).wrap(text=text)
+    return {
+        "lines": lines,
+        "font_size": font_size,
+        "height": get_text_height(lines[0], font_size),
+    }
+
+def add_text(meta, text):
+    text_color="white"
+    border_color="black"
+    border_size=2 # XXX Dynamic
+
+    line_data = calculate_lines(text, meta['x2'] - meta['x'], meta['y2'] - meta['y'])
+    lines = line_data['lines']
+    font_size = line_data['font_size']
+    height = line_data['height']
+    font = ImageFont.truetype(FONT, font_size)
+    for i in range(len(lines)):
+        line = lines[i]
+        # XXX: Center text
+        y = meta['y'] + (i * height)
+        x = meta['x']
+
+        draw.text((x-border_size, y-border_size), line, font=font, fill=border_color)
+        draw.text((x+border_size, y-border_size), line, font=font, fill=border_color)
+        draw.text((x-border_size, y+border_size), line, font=font, fill=border_color)
+        draw.text((x+border_size, y+border_size), line, font=font, fill=border_color)
+        draw.text((x, y), line, fill=text_color, font=font)
+
+for txt in filler_text:
+    for meta in texts:
+        if meta['field'] == txt['field']:
+            add_text(meta, txt['text'])
+            break
+
+img.show()
